7ff33ed8e455d5590e5bfef1cf71b02b0bfc4a6f
nicolacaddeo <nicola.caddeo58@gmail.com>
2022-08-23 18:54:56 +0200

    added ticket cost calculation

--- script.js
+++ script.js
@@ -11,16 +11,29 @@ if ((isNaN(distanceAsInt))) {
     document.getElementById('distance').innerHTML = 'Puoi inserire solo numeri';
 
 } else {
-    document.getElementById('distance').innerHTML = `Devi percorrere ${distanceAsInt} Km`
+    document.getElementById('distance').innerHTML = `Devi percorrere ${distanceAsInt} Km`;
 }
 
 if ((isNaN(passengerAgeInt))) {
     document.getElementById('age').innerHTML = 'Puoi inserire solo numeri';
 
 } else {
-    document.getElementById('age').innerHTML = `Hai ${passengerAgeInt} anni`
+    document.getElementById('age').innerHTML = `Hai ${passengerAgeInt} anni`;
 }
 
+// calcolo prezzo totale
 
+let totalPrice = (distance * 0.21);
+
+if (passengerAgeInt < 18) {
+    totalPrice = ((distance * 0.21) - ((totalPrice * 20) / 100));
+}
+console.log(totalPrice);
+
+if (passengerAgeInt > 65) {
+    totalPrice = ((distance * 0.21) - ((totalPrice * 40) / 100));
+}
+
+document.getElementById('price').innerHTML = `Il tuo biglietto costa ${totalPrice} euro`
 
 
